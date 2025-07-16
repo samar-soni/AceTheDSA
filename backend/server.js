5c97fd360efad3fa4ef206568236e68b004ba90e
Samar Soni <112100068+samar-soni@users.noreply.github.com>
2025-07-16 10:06:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user',userRouter);
-app.use('/api/user',markQuestionRouter);
+// app.use('/api/user',markQuestionRouter);
 app.use('/api/user', markQuestionRouter);
 app.use('/api/user',deleteQuestionRouter);
 
